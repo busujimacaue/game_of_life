347e4491695ef75212b1a661fd4015b9c336378b
Cauegz <66324617+busujimacaue@users.noreply.github.com>
2021-05-31 19:40:48 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("My First Game")
+
+while True:
+    pass
